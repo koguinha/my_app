e37918fb99a58ddfe1eeb74ced9c99e8c4914dcf
AKK <koga@akk-i.com>
2019-09-30 22:23:33 +0900

    from vscode

--- lib/answer.dart
+++ lib/answer.dart
@@ -11,7 +11,7 @@ class Answer extends StatelessWidget {
     return Container(
       width: double.infinity,
       child: RaisedButton(
-        color: Colors.red,
+        color: Colors.blue,
         textColor: Colors.white,
         child: Text(answerText),
         onPressed: selectHandler,
